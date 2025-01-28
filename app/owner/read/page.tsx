59fdd26e436ec229928d86bb06c671b2a7e426df
can <eee.gunescangorkem@gmail.com>
2025-01-28 17:56:33 +0300

    requests

--- app/owner/read/page.tsx
+++ app/owner/read/page.tsx
@@ -6,18 +6,27 @@ const Page = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [response, setRespone] = useState();
-  const login = async () => {
-    const res = await fetch("http://localhost:3001/api/register", {
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-      method: "POST",
+  const user = async () => {
+    const res = await fetch("http://localhost:3001/api/members", {
+      headers: {
+        Authorization:
+          "BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhbmdvcmtlbWd1bmVzQGdtYWlsLmNvbSIsImlkIjoiZ0JRN0RFbnpUVktKWmZjb3Y4ZnkiLCJpYXQiOjE3MzA4NjM2OTIsImV4cCI6MTczMzQ1NTY5Mn0.xb5oi0yRaZCweo542HieskEo0OgVGpt3FYp_fqn8h2Y",
+      },
+      method: "GET",
+
     });
+    /*       body: JSON.stringify([
+      { name: "can", lastName: "asdf" },
+      { name: "cihan", lastName: "senyuz" },
+    ]), */
+
+    if (res.ok) {
+      console.log(await res.json());
+    }
   };
   return (
     <div className="w-full h-screen flex justify-center items-center gap-8">
-      <form className="flex flex-col gap-8" action={login}>
+      <form className="flex flex-col gap-8" action={user}>
         <input
           type="text"
           value={email}
